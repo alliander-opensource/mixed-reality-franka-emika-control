d54a5e71a3340af78a89ad6ba5ed83df7ebe8f57
koen8100 <95222882+koen8100@users.noreply.github.com>
2023-06-02 17:48:59 +0200

    Got hand coords sent to topic

--- MRIRAC_Unity/Assets/MRIRAC/Scripts/HandPosePublisher.cs
+++ MRIRAC_Unity/Assets/MRIRAC/Scripts/HandPosePublisher.cs
@@ -3,32 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.Utilities;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Geometry;
 
 public class HandPosePublisher : MonoBehaviour
 {
+    private IMixedRealityHand handRight;
+    private MixedRealityPose pose;
 
-    public IMixedRealityHand handRight;
-    public MixedRealityPose pose;
+    ROSConnection ros;
+    [SerializeField]
+    private string HandPosePublisherTopic;
+
+    
 
     // Start is called before the first frame update
-    //void Start()
-    //{
-    //    
-    //}
+    void Start()
+    {
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<PoseMsg>(HandPosePublisherTopic);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (handRight == null)
         {
-            handRight = HandJointUtils.FindHand(Handedness.Right);
+            handRight = HandJointUtils.FindHand(Handedness.Left);
             Debug.Log("Finding hand...");
         }
         else 
         {
             handRight.TryGetJoint(TrackedHandJoint.Palm, out MixedRealityPose pose);
-            Debug.Log(pose);
-            //Debug.Log(pose.rotation);
+
+            PoseMsg handPose = new PoseMsg()
+            {
+                position = new PointMsg(pose.Position[0], pose.Position[1], pose.Position[2]),
+                orientation = new QuaternionMsg(pose.Rotation[0], pose.Rotation[1], pose.Rotation[2], pose.Rotation[3])
+            };
+            ros.Publish(HandPosePublisherTopic, handPose);
         }
+
+        
     }
 }
