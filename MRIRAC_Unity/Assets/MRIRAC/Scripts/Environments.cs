499aae600ed9ff5fb4891a06e32c45516425bf61
koen8100 <95222882+koen8100@users.noreply.github.com>
2023-08-08 17:05:56 +0200

    Enabled Environment spawning
    Made it possible to easily change the experiment environment by clicking on the condition button

--- MRIRAC_Unity/Assets/MRIRAC/Scripts/Environments.cs
+++ MRIRAC_Unity/Assets/MRIRAC/Scripts/Environments.cs
@@ -2,8 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Mrirac;
+
 public class Environments : MonoBehaviour
 {
+    [SerializeField]
+    private string hologramObstacleTopic;
+
+    // ROS Connector
+    ROSConnection ros;
+
+    [SerializeField]
+    private GameObject robot;
+
+    MeshFilter[] hologramObstacleMeshFilters;
+
+    [SerializeField]
+    private GameObject EnvironmentsObject;
+
+    [SerializeField]
+    private Vector3 DistanceFromRobotArm;
 
     [SerializeField]
     private GameObject StartAndGoalEnvironmentObjects;
@@ -23,7 +42,8 @@ public class Environments : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<MeshObstaclesMsg>(hologramObstacleTopic);
     }
 
     // Update is called once per frame
@@ -32,21 +52,38 @@ public class Environments : MonoBehaviour
         
     }
 
+    void PublishHologramMeshes()
+    {
+        hologramObstacleMeshFilters = EnvironmentsObject.GetComponentsInChildren<MeshFilter>();
+        MeshObstacleMsg[] hologramObstacleMsgs = new MeshObstacleMsg[hologramObstacleMeshFilters.Length];
+
+        int obstacleIdx = 0;
+        foreach (var hologramFilter in hologramObstacleMeshFilters)
+        {
+            MeshObstacleMsg meshObstacleMsg = new MeshObstacleMsg
+            {
+                mesh = MriracUtilities.MeshToMeshMsg(hologramFilter.mesh, hologramFilter.gameObject),
+                mesh_pose = MriracUtilities.PoseInRobotFrameMsg(robot, hologramFilter.gameObject),
+                name = hologramFilter.gameObject.name
+            };
+
+            hologramObstacleMsgs[obstacleIdx++] = meshObstacleMsg;
+
+        }
+
+        ros.Publish(hologramObstacleTopic, new MeshObstaclesMsg(hologramObstacleMsgs));
+    }
+
     public void CreateStartAndGoalPosition()
     {
         GameObject StartPosition = Instantiate(StartPositionPrefab, StartAndGoalEnvironmentObjects.transform, false);
         StartPosition.name = "StartPosition";
-        // StartPosition.transform.SetParent(StartAndGoalEnvironmentObjects.transform, false);
-
         StartPosition.transform.localPosition = StartPositionCoordinates;
 
         GameObject GoalPosition = Instantiate(GoalPositionPrefab, StartAndGoalEnvironmentObjects.transform, false);
         GoalPosition.name = "GoalPosition";
-        Debug.Log(GoalPosition.transform.position);
-        // GoalPosition.transform.SetParent(StartAndGoalEnvironmentObjects.transform, false);
-
         GoalPosition.transform.localPosition = GoalPositionCoordinates;
-        Debug.Log(GoalPosition.transform.position);
+
     }
 
     public void DestroyStartAndGoalPosition()
@@ -54,6 +91,29 @@ public class Environments : MonoBehaviour
         foreach (Transform objectTransform in StartAndGoalEnvironmentObjects.transform)
         {
             Destroy(objectTransform.gameObject);
+        }
+    }
+
+    public void ActivateEnvironment(GameObject PrefabEnvironment)
+    {
+
+        foreach (Transform environmentTransform in EnvironmentsObject.transform)
+        {
+            Destroy(environmentTransform.gameObject);
+        }
+
+        GameObject Environment = Instantiate(PrefabEnvironment, EnvironmentsObject.transform, false);
+        Environment.name = "Environment";
+        Environment.transform.localPosition = DistanceFromRobotArm;
+
+        PublishHologramMeshes();
+    }
+
+    public void DestroyEnvironment()
+    {
+        foreach (Transform environmentTransform in EnvironmentsObject.transform)
+        {
+            Destroy(environmentTransform.gameObject);
         }
     }
 }
